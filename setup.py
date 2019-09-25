cce1bb4e4b3498f00dccf3b089ec395c500efa13
Bea Steers <bea.steers@gmail.com>
2019-09-25 19:18:46 -0400

    prepping for pypi

--- setup.py
+++ setup.py
@@ -11,10 +11,9 @@ if len(sys.argv) > 1 and sys.argv[1] == 'sdist':
     weight_files = []
 else:
     weight_files = [download.download_file(f, url) for f, url in download.FILES]
-    weight_files = [f for f in weight_files if f and os.path.splitext(f)[1] == '.h5']
 
 
-setuptools.setup(name='vggish_keras',
+setuptools.setup(name='vggish-keras',
                  version='0.0.1',
                  description='VGGish in Keras.',
                  long_description=open(
@@ -22,12 +21,25 @@ setuptools.setup(name='vggish_keras',
                 ).read().strip(),
                  author='Bea Steers',
                  author_email='bea.steers@gmail.com',
-                 # url='http://path-to-my-packagename',
+                 url='https://github.com/beasteers/VGGish',
                  packages=setuptools.find_packages(),
-                 # py_modules=['packagename'],
                  package_data={'vggish': weight_files},
-                 # include_package_data=True,
-                 install_requires=[],
+                 classifiers=[
+                     "License :: OSI Approved :: ISC License (ISCL)",
+                     "Programming Language :: Python",
+                     "Development Status :: 3 - Alpha",
+                     "Intended Audience :: Developers",
+                     "Topic :: Software Development",
+                     "Programming Language :: Python :: 3",
+                     "Programming Language :: Python :: 3.5",
+                     "Programming Language :: Python :: 3.6",
+                     "Programming Language :: Python :: 3.7",
+                 ],
+                 install_requires=[
+                    'numpy',
+                    'tensorflow<=1.14',
+                    'pumpp',
+                 ],
                  license='MIT License',
                  zip_safe=False,
                  keywords='vggish audio audioset keras tensorflow')
